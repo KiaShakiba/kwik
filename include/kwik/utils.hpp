95c3e6ba36b59fe75c51bd971dcdc5dbe818717b
Kia Shakiba <kiashakiba@protonmail.com>
2021-10-31 17:58:08 -0400

    Add random function

--- include/kwik/utils.hpp
+++ include/kwik/utils.hpp
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <vector>
+#include <random>
 
 namespace kwik {
 	namespace utils {
@@ -11,6 +12,19 @@ namespace kwik {
 
 		template <typename T>
 		T cast(std::string);
+
+		template <typename T = double>
+		T random(T min = 0.0, T max = 1.0) {
+			if (min > max) {
+				throw std::invalid_argument("Min must be less than max");
+			}
+
+			std::random_device rd;
+			std::mt19937 gen(rd());
+			std::uniform_real_distribution<double> dis (0.0, 1.0);
+
+			return dis(gen) * (max - min) + min;
+		}
 	};
 };
 
