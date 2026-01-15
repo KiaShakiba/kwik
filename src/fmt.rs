ae7dc2ec6274ae0e5b7ceba8313bd2b5103b2e3c
Kia Shakiba <kiashakiba@protonmail.com>
2026-01-15 09:34:25 -0500

    feat: improve fmt timespan efficiency

--- src/fmt.rs
+++ src/fmt.rs
@@ -64,48 +64,74 @@ pub fn memory(value: impl AsPrimitive<u64>, precision: Option<usize>) -> String 
 /// ```
 #[must_use]
 pub fn timespan(value: impl AsPrimitive<u64>) -> String {
-	let mut milliseconds: u64 = value.as_();
+	let mut ms: u64 = value.as_();
 
-	let days = milliseconds / 1000 / 60 / 60 / 24;
-	milliseconds -= days * 1000 * 60 * 60 * 24;
+	let days = ms / 1000 / 60 / 60 / 24;
+	ms -= days * 1000 * 60 * 60 * 24;
 
-	let hours = milliseconds / 1000 / 60 / 60;
-	milliseconds -= hours * 1000 * 60 * 60;
+	let hrs = ms / 1000 / 60 / 60;
+	ms -= hrs * 1000 * 60 * 60;
 
-	let minutes = milliseconds / 1000 / 60;
-	milliseconds -= minutes * 1000 * 60;
+	let mins = ms / 1000 / 60;
+	ms -= mins * 1000 * 60;
 
-	let seconds = milliseconds / 1000;
-	milliseconds -= seconds * 1000;
+	let secs = ms / 1000;
+	ms -= secs * 1000;
 
-	let mut formatted = String::new();
-	let mut started = false;
+	match (days, hrs, mins, secs, ms) {
+		(0, 0, 0, 0, ms) => format!("{ms}"),
+		(0, 0, 0, secs, ms) => format!("{secs}.{ms:03}"),
+		(0, 0, mins, secs, ms) => format!("{mins}:{secs:02}.{ms:03}"),
+		(0, hrs, mins, secs, ms) => format!("{hrs}:{mins:02}:{secs:02}.{ms:03}"),
+		(days, hrs, mins, secs, ms) => format!("{days}.{hrs:02}:{mins:02}:{secs:02}.{ms:03}"),
+	}
+}
 
-	if days > 0 {
-		formatted.push_str(&format!("{days}."));
-		started = true;
+#[cfg(test)]
+mod tests {
+	use crate::fmt;
+
+	#[test]
+	fn it_fmts_timespan_ms() {
+		assert_eq!(fmt::timespan(0), "0");
+		assert_eq!(fmt::timespan(1), "1");
+		assert_eq!(fmt::timespan(10), "10");
+		assert_eq!(fmt::timespan(100), "100");
 	}
 
-	if started || hours > 0 {
-		let padding = if started { 2 } else { 0 };
-		formatted.push_str(&format!("{hours:0padding$}:"));
-		started = true;
+	#[test]
+	fn it_fmts_timespan_secs() {
+		assert_eq!(fmt::timespan(1_000), "1.000");
+		assert_eq!(fmt::timespan(1_001), "1.001");
+		assert_eq!(fmt::timespan(10_000), "10.000");
+		assert_eq!(fmt::timespan(31_234), "31.234");
+		assert_eq!(fmt::timespan(59_999), "59.999");
 	}
 
-	if started || minutes > 0 {
-		let padding = if started { 2 } else { 0 };
-		formatted.push_str(&format!("{minutes:0padding$}:"));
-		started = true;
+	#[test]
+	fn it_fmts_timespan_mins() {
+		assert_eq!(fmt::timespan(60_000), "1:00.000");
+		assert_eq!(fmt::timespan(60_001), "1:00.001");
+		assert_eq!(fmt::timespan(600_000), "10:00.000");
+		assert_eq!(fmt::timespan(1_234_567), "20:34.567");
+		assert_eq!(fmt::timespan(3_599_999), "59:59.999");
 	}
 
-	if started || seconds > 0 {
-		let padding = if started { 2 } else { 0 };
-		formatted.push_str(&format!("{seconds:0padding$}."));
-		started = true;
+	#[test]
+	fn it_fmts_timespan_hrs() {
+		assert_eq!(fmt::timespan(3_600_000), "1:00:00.000");
+		assert_eq!(fmt::timespan(3_600_001), "1:00:00.001");
+		assert_eq!(fmt::timespan(36_000_000), "10:00:00.000");
+		assert_eq!(fmt::timespan(12_345_678), "3:25:45.678");
+		assert_eq!(fmt::timespan(86_399_999), "23:59:59.999");
 	}
 
-	let padding = if started { 3 } else { 0 };
-	formatted.push_str(&format!("{milliseconds:0padding$}"));
-
-	formatted
+	#[test]
+	fn it_fmts_timespan_days() {
+		assert_eq!(fmt::timespan(86_400_000), "1.00:00:00.000");
+		assert_eq!(fmt::timespan(86_400_001), "1.00:00:00.001");
+		assert_eq!(fmt::timespan(864_000_000), "10.00:00:00.000");
+		assert_eq!(fmt::timespan(123_456_789), "1.10:17:36.789");
+		assert_eq!(fmt::timespan(950_399_999), "10.23:59:59.999");
+	}
 }
